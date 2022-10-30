c0de3f5669ff759416998a63da4b0b28c4547c09
Miguelprz <migueleperezc@gmail.com>
2022-10-30 02:41:36 -0500

    Creacion de registros de usuarios

--- ecomerce/src/app/modules/pages/auth/signup/signup.component.ts
+++ ecomerce/src/app/modules/pages/auth/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Auth} from "@core/services/classes/Auth";
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +8,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  formR!: FormGroup;
+  image!: File;
 
-  constructor() { }
+  constructor(
+    private auth: Auth,
+    private formB: FormBuilder
+  ) { }
+
+  configForm():void {
+    this.formR = this.formB.group({
+      nuip:['', Validators.required],
+      nombres:['', Validators.required],
+      apellidos:['', Validators.required],
+      email:['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      passwordc: ['', Validators.required]
+    });
+  }
+
+  getImage(event: any): File {
+    this.image = event.target.files[0];
+    return this.image;
+  }
+
+  dataForm():FormData {
+    const formD = new FormData();
+    formD.append('nuip', this.formR.get('nuip')?.value);
+    formD.append('nombres', this.formR.get('nombres')?.value);
+    formD.append('apellidos', this.formR.get('apellidos')?.value);
+    formD.append('email', this.formR.get('email')?.value);
+    formD.append('password', this.formR.get('password')?.value);
+    formD.append('passwordc', this.formR.get('passwordc')?.value);
+    if(this.image){
+      formD.append('image', this.image);
+    }
+    return formD;
+  }
 
   ngOnInit(): void {
+    this.configForm()
+  }
+
+  register():void{
+    this.auth.register(this.dataForm());
   }
 
 }
